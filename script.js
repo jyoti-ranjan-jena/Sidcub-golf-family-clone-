2abf5d22bb072675733a3a6ae15ed42aa5613175
Jyoti Ranjan Jena <144580785+jyoti-ranjan-jena@users.noreply.github.com>
2024-02-17 13:56:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+var cur = document.querySelector("#cursor");
+var curBlur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", function(dets){
+    cur.style.left = dets.x + "px"
+    cur.style.top = dets.y + "px"
+    curBlur.style.left = dets.x - 250 + "px"
+    curBlur.style.top = dets.y - 250 + "px"
+})
+
+var menuele = document.querySelectorAll('#nav h4')
+menuele.forEach(function(ele){
+    ele.addEventListener("mouseenter",function(){
+        cur.style.scale = 3
+        cur.style.border = "1px solid #fff"
+        cur.style.backgroundColor = "transparent"
+    })
+    ele.addEventListener("mouseleave",function(){
+        cur.style.scale=1;
+        cur.style.border="0px solid #92c11e";
+        cur.style.backgroundColor="#92c11e";
+    })
+})
+
+
+
+gsap.to("#nav",{
+    backgroundColor : "#000",
+    duration : 0.5,
+    height : "110px",
+    scrollTrigger:{
+        trigger : "#nav",
+        scroller : "body",
+        // markers : true,
+        start : "top -10%",
+        end : "top -11%",
+        scrub : 1
+    }
+})
+
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger : {
+        trigger : "#main",
+        scroller : "body",
+        // markers : true,
+        start : "top -25%",
+        end : "top -70%",
+        scrub : 2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in",{
+    y: 90,
+    opacity : 0,
+    stagger : 0.2,
+    duration: 0.4,
+    scrollTrigger: {
+        trigger:"#about-us",
+        scroller : "body",
+        // markers: true,
+        start : "top 70%",
+        end : "top 65%",
+        scrub : 2
+    },
+});
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity : 0,
+    duration: 0.4,
+    stagger:0.1,
+    scrollTrigger: {
+        trigger:".card",
+        scroller : "body",
+        // markers: true,
+        start : "top 70%",
+        end : "top 65%",
+        scrub : 1
+    },
+});
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller : "body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller : "body",
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller : "body",
+        start:"top 75%",
+        end:"top 70%",
+        scrub:2
+    }
+})
